a2e94df018931253cd7a07734b81e69d35990252
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-06 14:20:24 +0200

    added 1st button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,6 +18,32 @@ const Hero = () => {
                   Design System
                 </h2>
               </div>
+              <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold tracking-tight text-neutral-900 dark:text-white">
+                <span className="block mb-1">Design</span>
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-amber-500 via-amber-400 to-amber-600">
+                  Without Limits
+                </span>
+              </h1>
+              <p className="text-base lg:text-lg text-neutral-700 dark:text-neutral-300 max-w-2xl font-light">
+                Elevate your web projects with our stremlines design system
+                powered by the latest Tailwind CSS V4. Create stunning user
+                experinces with state of he art theme switching.
+              </p>
+              <div className="flex gap-3 items-center">
+                <div className="h-px bg-neutral-300 dark:bg-neutral-700 w-12"></div>
+                <p className="uppercase text-xs tracking-widest text-neutral-500 dark:text-neutral-400 font-medium ">
+                  Transition seamlessly
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-3 lg:gap-4">
+              <button className="px-6 py-3 lg:px-7 lg:py-3.5 rounded-lg bg-neutral-900 hover:bg-black text-white dark:bg-amber-500 dark:hover:bg-amber-600 dark:text-neutral-900 font-medium shadow-lg
+              hover:shadow-neutral-900/20 dark:hover:shadow-amber-500/20 flex items-center transition group">
+                <i className="bx bx-code-alt mr-2 lg:mr-3 text-lg lg:text-xl group-hover:rotate-12 transition-transform">
+                  <span>Start Coding</span>
+                </i>
+              </button>
+             
             </div>
           </div>
         </div>
