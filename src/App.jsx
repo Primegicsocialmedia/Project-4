2664b746da74098056a247ea86cd4400f2dfc9ed
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-06 11:08:19 +0200

    added sun to darkmode-lightmode

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ const App = () => {
   };
 
   return (
-    <div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 relative">
+    <div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 relative transition-colors duration-300">
       <button
         onClick={toggleDarkMode}
         className="fixed top-3 lg:top-4 right-3 lg:right-4 w-8 h-9 lg:w-10 lg:h-10 flex justify-center items-center rounded-full bg-amber-500 text-neutral-950 shaow-lg hover:bg-amber-600 transition-colors"
       >
-        <i className="bx bx-moon text-lg lg:text-xl"></i>
+        <i className={`bx bx-${darkMode ? "sun" : "moon"} text-lg lg:text-xl`}></i>
       </button>
       <Hero />
     </div>
