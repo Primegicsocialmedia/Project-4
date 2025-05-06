a68ce1dea855eebc0598354385ef90d08e5914c4
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-06 10:40:27 +0200

    dark light mode completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import Hero from './components/Hero'
+import React, { useState, useEffect } from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return (
-    <div >
-      <Hero/>
-      
-    </div>
-  )
-}
+  const [darkMode, setdarkMode] = useState(false);
 
-export default App
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setdarkMode(!darkMode);
+  };
+
+  return (
+    <div className="min-h-screen bg-neutral-100 dark:bg-neutral-950 relative">
+      <button
+        onClick={toggleDarkMode}
+        className="fixed top-3 lg:top-4 right-3 lg:right-4 w-8 h-9 lg:w-10 lg:h-10 flex justify-center items-center rounded-full bg-amber-500 text-neutral-950 shaow-lg hover:bg-amber-600 transition-colors"
+      >
+        <i className="bx bx-moon text-lg lg:text-xl"></i>
+      </button>
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
